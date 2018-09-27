9197485f19f430b6eb83e150b7ecc996d070b7ec
Ziconin <zcaraic@gmail.com>
2018-09-27 15:00:35 +0300

    Removed projects

--- Bloglist/front-end/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/login'
-
-const login = async (creds) => {
-  const res = await axios.post(baseUrl, creds)
-  return res.data
-}
-
-export default {login}
